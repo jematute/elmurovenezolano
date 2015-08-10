4c1d0a8aca1b8e6bf87b59e8da1208a86e93f310
jematute <juan.matute@gmail.com>
2015-08-10 07:26:16 -0400

    Changed some of the wording on the main page.

--- app/Listado/listado.js
+++ app/Listado/listado.js
@@ -15,9 +15,11 @@
         vm.map = { center: { latitude: 6.327618611972444, longitude: -66.44843750000001 }, zoom: 6 };
 
         vm.rowCollection = [
-        { firstName: 'Laurent', lastName: 'Renard', birthDate: new Date('1987-05-21'), balance: 102, email: 'whatever@gmail.com' },
-        { firstName: 'Blandine', lastName: 'Faivre', birthDate: new Date('1987-04-25'), balance: -2323.22, email: 'oufblandou@gmail.com' },
-        { firstName: 'Francoise', lastName: 'Frere', birthDate: new Date('1955-08-27'), balance: 42343, email: 'raymondef@gmail.com' }
+            { estado: "Apure"},
+            { estado: "Aragua" },
+            { estado: "Carbobo" },
+            { estado: "Cojedes" },
+            { estado: "Guarico" },
         ];
     }
 })();
